b82e810784361345c5c78090feae7e6f5c0cbc49
Vincent Lore <vincent_lore@hotmail.com>
2016-11-16 03:01:45 -0800

    Generate players component

---
+++ src/app/players/players.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'players',
+  templateUrl: './players.component.html',
+  styleUrls: ['./players.component.css']
+})
+export class PlayersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
