78c796b4dccfaaa13278f23451d15c7b7e7e1d26
Vincent Lore <vincent_lore@hotmail.com>
2016-11-20 04:21:54 -0800

    Add players component

--- src/app/players/players.component.ts
+++ src/app/players/players.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { PlayersService } from '../services/players.service';
+
+import { Player } from '../models/player';
 
 @Component({
-  selector: 'players',
-  templateUrl: './players.component.html',
-  styleUrls: ['./players.component.css']
+    selector: 'players',
+    templateUrl: './players.component.html',
+    styleUrls: ['./players.component.css'],
+    providers: [PlayersService],
 })
 export class PlayersComponent implements OnInit {
 
-  constructor() { }
+    players: Player[];
+    errorMsg: string;
 
-  ngOnInit() {
-  }
+    constructor(private playersService: PlayersService) { }
+
+    ngOnInit() {
+        this.getPlayers();
+    }
+
+    getPlayers() {
+        this.playersService.getPlayers()
+            .subscribe(
+                players => this.players = players,
+                error => this.errorMsg = error
+            );
+    }
 
 }
---
+++ src/app/services/players.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, Response } from '@angular/http';
+import { Observable } from 'rxjs';
+
+import { Player } from '../models/player';
+
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class PlayersService {
+
+    players: Player[] = [];
+
+    constructor(private http: Http) { }
+
+    getPlayers(): Observable<Player[]> {
+        return this.http.get('http://localhost:8000/api/players')
+            .map(response => {
+                for (let player of response.json()) {
+                    this.players.push(new Player(player));
+                }
+                return this.players;
+            }).catch(error => {
+                console.log(error);
+                return Observable.throw(error);
+            })
+    }
+
+}
