555a5117a7fdfef8eb43edc8420d3af5fbe809b1
Vincent Lore <vincent_lore@hotmail.com>
2016-11-20 22:32:41 -0800

    Fix casing warning messages

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
 
-import { AuthService } from '../services/Auth.service';
+import { AuthService } from '../services/auth.service';
 
 @Component({
     selector: 'app-login',
