9ea3bbb82e988503475808479cd6ef9a0fbb7c5a
Vincent Lore <vincent_lore@hotmail.com>
2016-11-23 00:51:36 -0800

    Implement teams component with styled view

--- src/app/teams/teams.component.ts
+++ src/app/teams/teams.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { TeamsService } from '../services/teams.service';
+
+import { Team } from '../models/team';
 
 @Component({
-  selector: 'app-teams',
-  templateUrl: './teams.component.html',
-  styleUrls: ['./teams.component.css']
+    selector: 'app-teams',
+    templateUrl: './teams.component.html',
+    styleUrls: ['./teams.component.css'],
+    providers: [TeamsService],
 })
 export class TeamsComponent implements OnInit {
 
-  constructor() { }
+    category: String = 'league';
+    teams: Team[] = [];
 
-  ngOnInit() {
-  }
+    errorMsg: string;
+
+    constructor(private teamsService: TeamsService) { }
+
+    ngOnInit() {
+        this.getTeams();
+    }
+
+    getTeams() {
+        return this.teamsService.getTeams()
+            .subscribe(
+            teams => this.teams = teams,
+            error => this.errorMsg = error
+            )
+    }
 
 }
