e5dee49871d91b6704f26e6c3d2881a57fb1270e
Vincent Lore <vincent_lore@hotmail.com>
2016-11-25 23:33:12 -0800

    Add form in side menu to create custom team
    Moved side menu button container outside of nav list.
    Consequently, generalized CSS classes

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 import { AuthService } from '../services/auth.service';
+import { CustomTeamService } from '../services/custom-team.service';
 
 @Component({
     selector: 'nav-menu',
     templateUrl: './menu.component.html',
-    styleUrls: ['./menu.component.css']
+    styleUrls: ['./menu.component.css'],
+    providers: [CustomTeamService]
 })
 export class MenuComponent implements OnInit {
 
     loggedIn: boolean;
     subscription: Subscription;
     username: string;
+    errorMsg: string;
+    teamName: string;
 
-    constructor(private authService: AuthService) { }
+    constructor(
+        private authService: AuthService, 
+        private customTeamService: CustomTeamService, 
+        private router: Router
+    ) { }
 
     ngOnInit() {
         this.subscription = this.authService.isLoggedIn$
@@ -27,6 +37,16 @@ export class MenuComponent implements OnInit {
 
     ngOnDestroy() {
         this.subscription.unsubscribe();
+    } 
+
+    createTeam(f: NgForm) {
+        if (f.valid) {
+            this.customTeamService.createTeam(this.teamName)
+                .subscribe(
+                    team => this.router.navigate(['/custom-teams', team.id]),
+                    error => this.errorMsg = error
+                )
+        }
     }
 
 }
