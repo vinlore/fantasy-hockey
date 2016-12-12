904f5856dcc10263dbac9781102663d3f3d8bc21
Vincent Lore <vincent_lore@hotmail.com>
2016-12-11 23:51:15 -0800

    Implement adding player to custom teams

--- src/app/players/players.component.ts
+++ src/app/players/players.component.ts
@@ -1,36 +1,71 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 import { PlayerService } from '../services/player.service';
+import { CustomTeamService } from '../services/custom-team.service';
+import { AuthService } from '../services/auth.service';
 
 import { Player } from '../models/player';
+import { CustomTeam } from '../models/custom-team';
 
 @Component({
     selector: 'players',
     templateUrl: './players.component.html',
     styleUrls: ['./players.component.css'],
-    providers: [PlayerService],
+    providers: [PlayerService, CustomTeamService],
 })
 export class PlayersComponent implements OnInit {
 
+    loggedIn: boolean;
+    subscription: Subscription;
     playersSize: number = 0;
     pageSize: number = 30;
     page: number = 1;
     players: Player[] = [];
     errorMsg: string;
+    teams: CustomTeam[];
 
-    constructor(private playerService: PlayerService) { }
+    constructor(
+        private playerService: PlayerService,
+        private customTeamService: CustomTeamService,
+        private authService: AuthService
+    ) { }
 
     ngOnInit() {
         this.getPlayers();
+        this.subscription = this.authService.isLoggedIn$
+            .subscribe(loggedIn => {
+                this.loggedIn = loggedIn;
+                if (loggedIn) {
+                    this.getTeams();
+                }
+            });
+        if (this.loggedIn) {
+            this.getTeams();
+        }
     }
 
     getPlayers() {
         this.playerService.getPlayers()
             .subscribe(
-                players => { this.players = players; this.playersSize = this.players.length },
-                error => this.errorMsg = error
+            players => { this.players = players; this.playersSize = this.players.length },
+            error => this.errorMsg = error
             );
+    }
+
+    getTeams() {
+        this.customTeamService.getTeams()
+            .subscribe(
+            teams => this.teams = teams,
+            error => this.errorMsg = error
+            )
+    }
+
+    addPlayer(team: CustomTeam, player: Player) {
+        this.customTeamService.addPlayer(team, player)
+            .subscribe(
+                result => {}
+            )
     }
 
 }
--- src/app/services/custom-team.service.ts
+++ src/app/services/custom-team.service.ts
@@ -7,6 +7,7 @@ import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 import { CustomTeam } from '../models/custom-team';
+import { Player } from '../models/player';
 
 @Injectable()
 export class CustomTeamService {
@@ -41,6 +42,17 @@ export class CustomTeamService {
         return this.http.get('http://localhost:8000/api/custom-teams/' + id)
             .map(response => {
                 return new CustomTeam(response.json());
+            }).catch(error => {
+                console.log(error);
+                return Observable.throw(error);
+            })
+    }
+
+    addPlayer(team: CustomTeam, player: Player) {
+        let url = 'http://localhost:8000/api/custom-teams/' + team.id + '/players/' + player.id;
+        return this.http.post(url, {})
+            .map(response => {
+                return response.json().data;
             }).catch(error => {
                 console.log(error);
                 return Observable.throw(error);
